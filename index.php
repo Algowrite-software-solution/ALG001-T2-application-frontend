c7bae396ed8459387f269793f9592089b11708da
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-07-02 01:06:40 +0530

    file structure added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALG001</title>
+</head>
+
+<body>
+
+    <!-- your application UI should go here -->
+
+</body>
+
+</html>
