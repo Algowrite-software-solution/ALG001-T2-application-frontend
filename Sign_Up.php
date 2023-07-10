19b9414961626e85f8e7a9752b919443ec2265d9
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-07-11 01:55:47 +0530

    signup interface

---
+++ Sign_Up.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&display=swap" rel="stylesheet">
+    <title>Sign-Up</title>
+</head>
+
+<body>
+    <section class="section1 bg-dark">
+        <div class="content py-4">
+            <div class="row  d-flex justify-content-center  m-0">
+                <div class="box col-8 rounded-5">
+                    <div class="row m-0 h-100 d-flex justify-content-center align-content-center">
+                        <div class="signup_heading col-8 text-light text-center fs-2">Instargram pro</div>
+                        <div class="col-8 text-light text-center">Sign up to see photos and videos from your friends.</div>
+                        <!-- first button set -->
+                        <div class="row m-0 d-flex justify-content-center pt-5 pb-3 gap-3">
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="text" placeholder="username" name="username">
+                            </div>
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="text" placeholder="Display Name" name="Display Name">
+                            </div>
+                        </div>
+                        <!-- second button set -->
+                        <div class="row m-0 d-flex justify-content-center pb-3 gap-3">
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="email" placeholder="email" name="email">
+                            </div>
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="date" placeholder="Birth Day" name="Birth Day">
+                            </div>
+                        </div>
+                        <!-- third button set -->
+                        <div class="row m-0 d-flex justify-content-center pb-3 gap-3">
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="password" placeholder="password" name="password">
+                            </div>
+                            <div class="col-5 text-center">
+                                <input class="signup_input form-control bg-transparent text-light border-3" type="password" placeholder="confirm password" name="confirm password">
+                            </div>
+                        </div>
+                        <!-- sign up button -->
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-4 d-grid py-4">
+                                <input type="submit" class="signup_btn text-light" value="Sign Up">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+</body>
+
+</html>
