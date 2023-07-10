d97e04fdd80e814f8fafa1bca970ad35a7248cd2
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-07-11 01:59:59 +0530

    ui developed

---
+++ sign_in.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALG001 - Sign In</title>
+
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- js scripts -->
+    <script src="js/script.js" defer></script>
+</head>
+
+<body class="body" style="height: 100vh;">
+
+    <!-- your application UI should go here -->
+    <section class="d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 p-0 h-100">
+            <div class="row m-0 d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-6 col-lg-5 col-xl-4 p-0">
+                    <div class="row m-0 px-3 d-flex justify-content-center text-center">
+                        <h1 class="greeting-title">Alg001</h1>
+
+                        <hr class="c-line">
+
+                        <h3 class="fs-6 text-white fw-normal">Hi! welcome User Name</h3>
+
+                        <div class="d-flex flex-column my-4">
+                            <input class="c-input-border" type="text" placeholder="Username or Email">
+                            <input class="c-input-border" type="text" placeholder="Password">
+                            <button class="c-btn">Sign In</button>
+                        </div>
+
+                        <div>
+                            <a class="text-white" href="#">Forget Password?</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
